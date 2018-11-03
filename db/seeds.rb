904fa19c51c068f2d4ae761145412c2acdce41cd
Roger Garcia <r1ger_pg@hotmail.com>
2018-11-03 18:17:07 +0800

    Basic MVC

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+PRICES = [70,75,80,85,90,95]
+GUESTS = [1,2,3,4]
+
+NAMES = [
+'Light & Spacious Garden Flat London',
+'Cozy and beautiful house Sydney',
+'Bohemian pen house in Paris',
+'Modern and sophisticated flat in Tokio',
+'Urban and contemporary semi-dettached house in New York',
+'Traditional Bavarian large house Munich',
+'Leafy apartment with great view of city center in Barcelona'
+]
+
+puts 'Started seeding'
+9.times do
+  puts 'One flat'
+Flat.create!(
+  name: NAMES.sample,
+  address: Faker::Address.street_address,
+  description: Faker::TwinPeaks.quote,
+  price_per_night: PRICES.sample,
+  number_of_guests: GUESTS.sample
+)
+end
+puts 'Finished seeding'
+
